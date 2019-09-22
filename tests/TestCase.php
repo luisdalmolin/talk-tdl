98d871f4550bb629793334b2a2422e084d01d7dd
Luis Dalmolin <luis.nh@gmail.com>
2019-09-22 09:14:16 -0300

    Added signIn helper

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,19 @@
 
 namespace Tests;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, RefreshDatabase;
+
+    protected $user;
+
+    public function signIn()
+    {
+        $this->user = factory(User::class)->create();
+        return $this->actingAs($this->user);
+    }
 }
